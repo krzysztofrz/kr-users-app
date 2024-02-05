dc22b3e9cefb2bb770a3697ba32040a5a782c83e
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2024-02-05 16:11:24 +0100

    2. EsLint, prettier, husky

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,15 @@
-import logo from "../assets/logo.svg";
-import "./App.css";
+import logo from '../assets/logo.svg';
+import './App.css';
+
+console.log('moje');
 
 function App() {
-	return (
-		<div className="App">
-			<div>kr mine page</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div>kr mine page</div>
+      <img src="{logo}" alt="logo" />
+    </div>
+  );
 }
 
 export default App;
