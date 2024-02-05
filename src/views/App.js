de135ddfa69b1780167f7a9e1f3a5c6a4efd56b3
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2024-02-05 11:43:17 +0100

    1.  initial configuration

---
+++ src/views/App.js
@@ -0,0 +1,12 @@
+import logo from "../assets/logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div>kr mine page</div>
+		</div>
+	);
+}
+
+export default App;
