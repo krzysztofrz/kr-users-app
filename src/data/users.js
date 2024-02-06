fcc605c4994d91291865bcbb0e11322a6d3f0366
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2024-02-06 12:16:09 +0100

    users list - row structure

---
+++ src/data/users.js
@@ -0,0 +1,36 @@
+export const users = [
+  {
+    name: 'John Dow',
+    attendance: '39%',
+    average: '4,5',
+  },
+  {
+    name: 'Albert Ert',
+    attendance: '19%',
+    average: '4,5',
+  },
+  {
+    name: 'Thomas Cook',
+    attendance: '44%',
+    average: '3,4',
+  },
+  {
+    name: 'Jason Williams',
+    attendance: '22%',
+    average: '1,9',
+  },
+  {
+    name: 'Chris Webber',
+    attendance: '11%',
+    average: '2,5',
+  },
+  {
+    name: 'Steve Curry',
+    attendance: '23%',
+    average: '4,0',
+  },
+  {
+    name: 'Isaiah Atcin',
+    average: '3,5',
+  },
+];
